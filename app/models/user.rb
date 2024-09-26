24f379047a079e3c9d3bf7ea2acf3a5c55815f9c
Jack Mahony <jackmahonyy@gmail.com>
2024-09-26 11:32:27 +1000

    Added post and comments models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :firstname, presence: :true
+  validates :lastname, presence: :true
+  validates :email, presence: :true
+  validates :email, presence: :true
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
